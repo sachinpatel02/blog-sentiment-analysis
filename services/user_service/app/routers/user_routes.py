bdd71cadb78210bd0940e6e98f990c5556828bfb
sachinpatel02 <sachinp1613@gmail.com>
2025-08-03 21:37:18 +0530

    ✍🏻updating access token with user.id

--- services/user_service/app/routers/user_routes.py
+++ services/user_service/app/routers/user_routes.py
@@ -62,7 +62,7 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = security.create_access_token(data={"sub": user.email})
+    access_token = security.create_access_token(data={"sub": str(user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
 
 # 3. me
